da4bff2ccf1074ab3083106b1ca053dce46771fc
Enos-Vinicius <enosviniciusda@gmail.com>
2021-02-03 17:13:58 -0300

    importanto dependencias do openlayers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+  map;
+  
+  ngOnInit(){
+    this.map = new Map({
+      target: 'hotel_map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([-60.3444, -14.2160]),
+        zoom: 5
+      })
+    });
+  }
 }
