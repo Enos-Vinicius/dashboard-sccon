41add3e4bafbd5549cfebbe2947042addb02609a
Enos-Vinicius <enosviniciusda@gmail.com>
2021-02-04 17:33:58 -0300

    criado component de navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+  { path: 'landing-page', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { PageComponent } from './page/page.component';
+import { MaterialModule } from 'src/shared/material.module';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageComponent,
+    MainComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    LandingPageRoutingModule
   ]
 })
 export class LandingPageModule { }
