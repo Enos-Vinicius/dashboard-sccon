d0841a13e212903ed7f55779aa520e09df0e979c
Enos-Vinicius <enosviniciusda@gmail.com>
2021-02-08 14:48:19 -0300

    concluido criação de component saiba mais

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -6,13 +6,15 @@ import { PageComponent } from './page/page.component';
 import { MaterialModule } from 'src/shared/material.module';
 import { MainComponent } from './main/main.component';
 import { InfoSoftwareComponent } from './info-software/info-software.component';
+import { KnowMoreComponent } from './know-more/know-more.component';
 
 
 @NgModule({
   declarations: [
     PageComponent,
     MainComponent,
-    InfoSoftwareComponent
+    InfoSoftwareComponent,
+    KnowMoreComponent
   ],
   imports: [
     CommonModule,
