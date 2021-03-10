db983caa6c223344be63324f9707fe406b788a00
Enos-Vinicius <enosviniciusda@gmail.com>
2021-03-10 16:18:33 -0300

    criado página de noticias

--- src/app/landing-page/info-news/info-news.component.ts
+++ src/app/landing-page/info-news/info-news.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class InfoNewsComponent implements OnInit {
 
   arrNews = [
-    {title: "Polícia Federal combate crimes ambientais em assentamentos do INCRA no Acre", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "GOV.BR"},
-    {title: "Operação da PF faz a maior apreensão de madeira da história", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "FOLHA DE S.PAULO"},
-    {title: "Ministério da Justiça e Segurança Pública institui Programa Brasil MAIS como um dos projetos estratégicos da Pasta", description: "Medida aplica geotecnologia em apoio às funções de segurança pública com finalidade e objetivos relacionados ao MJSP", origin: "GOV.BR"},
-    {title: "Polícia Federal combate crimes ambientais em assentamentos do INCRA no Acre", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "GOV.BR"},
-    {title: "Operação da PF faz a maior apreensão de madeira da história", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "FOLHA DE S.PAULO"},
-    {title: "Ministério da Justiça e Segurança Pública institui Programa Brasil MAIS como um dos projetos estratégicos da Pasta", description: "Medida aplica geotecnologia em apoio às funções de segurança pública com finalidade e objetivos relacionados ao MJSP", origin: "GOV.BR"},
+    {order: "2", order_img: "1", title: "Polícia Federal combate crimes ambientais em assentamentos do INCRA no Acre", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "GOV.BR"},
+    {order: "1", order_img: "2", title: "Operação da PF faz a maior apreensão de madeira da história", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "FOLHA DE S.PAULO"},
+    {order: "2", order_img: "1", title: "Ministério da Justiça e Segurança Pública institui Programa Brasil MAIS como um dos projetos estratégicos da Pasta", description: "Medida aplica geotecnologia em apoio às funções de segurança pública com finalidade e objetivos relacionados ao MJSP", origin: "GOV.BR"},
+    {order: "1", order_img: "2", title: "Polícia Federal combate crimes ambientais em assentamentos do INCRA no Acre", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "GOV.BR"},
+    {order: "2", order_img: "1", title: "Operação da PF faz a maior apreensão de madeira da história", description: "Batizada de Handroanthus GLO, ação reteve 131,1 mil metros cúbicos de toras extraídas no oeste do Pará", origin: "FOLHA DE S.PAULO"},
+    {order: "1", order_img: "2", title: "Ministério da Justiça e Segurança Pública institui Programa Brasil MAIS como um dos projetos estratégicos da Pasta", description: "Medida aplica geotecnologia em apoio às funções de segurança pública com finalidade e objetivos relacionados ao MJSP", origin: "GOV.BR"},
   ]
 
   constructor() { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apis: {
+    pf: "https://plataforma-pf.sccon.com.br/gama-api"
+  }
 };
