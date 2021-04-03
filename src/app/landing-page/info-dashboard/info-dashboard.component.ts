861f33d0b82742aab9d27fbe6f8c2e9e56e061f1
Enos-Vinicius <enosviniciusda@gmail.com>
2021-04-03 00:02:24 -0300

    concluido parallax info-dashboard

--- src/app/landing-page/info-dashboard/info-dashboard.component.ts
+++ src/app/landing-page/info-dashboard/info-dashboard.component.ts
@@ -11,24 +11,31 @@ export class InfoDashboardComponent implements OnInit {
 
   @ViewChild('divMackbook', {static: true}) divMackbook: ElementRef;
   @ViewChild('divIphone', {static: true}) divIphone: ElementRef;
+  lastScrollTop = 0;
 
   constructor(
     public authService : AuthService,
     private renderer: Renderer2
   ) { }
 
-  ngOnInit(): void {
-    this.renderer.listen('window', 'scroll', (event) => {
-      const number = window.scrollY;
-
-      if(Helper.widthTablet()){
-        if(number > 4000 && number < 5200){
-          this.divMackbook.nativeElement.style.marginRight = (5500 - (number + 300)) + 'px';
-        } 
-      } else if(!Helper.widthPhone()){
-        if(number > 500 && number < 3900){
-          this.divMackbook.nativeElement.style.marginLeft = -(4100 - number) + 'px';
-        }    
+  ngOnInit(): void {  
+    this.renderer.listen('window', 'scroll', (event) => { 
+      if(window.scrollY > this.lastScrollTop){
+        this.lastScrollTop = window.scrollY;
+        const number = window.scrollY;
+    
+        if(Helper.widthTablet()){
+          if(number > 4000 && number < 5200){
+            this.divMackbook.nativeElement.style.marginRight = (5500 - (number + 300)) + 'px';
+          } 
+        } else if(!Helper.widthPhone()){
+            if(number > 500 && number < 4052){
+              this.divMackbook.nativeElement.style.marginLeft = -(4100 - number) + 'px';      
+            }    
+            if(number > 3800 && number < 4000){
+              this.divIphone.nativeElement.style.marginLeft = (4000 - number) + 'px';
+            }
+        }
       }
 
 
